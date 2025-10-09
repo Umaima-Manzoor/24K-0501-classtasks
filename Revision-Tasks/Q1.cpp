26c7115eef18bced54fc28f1b3b649d67e906f5e
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-10-09 15:26:46 +0500

    Create Q1.cpp

---
+++ Revision-Tasks/Q1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void ShellSort(int arr[], int n) {
+    for (int gap = n/2; gap>0; gap/=2) {
+
+        for (int j=gap; j<n; j++) {
+            int temp = arr[j];
+            int res = j;
+
+            while(res>=gap && arr[res-gap]>temp) {
+                arr[res] = arr[res-gap];
+                res = res - gap;
+            }
+            arr[res] = temp;
+        }
+
+        cout << "After gap = " << gap << ": ";
+        for (int i=0; i<6; i++) {
+        cout<<arr[i]<<" ";
+        }
+        cout << endl<< endl;
+    }
+}
+
+
+int main () {
+
+    int arr[] = {205, 102, 310, 450, 120, 90};
+
+    ShellSort(arr, 6);
+
+    cout << "Sorted array: ";
+    for (int i=0; i<6; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+}
